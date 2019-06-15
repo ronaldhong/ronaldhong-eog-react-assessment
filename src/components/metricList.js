97720238f62cc8d4a3d19de032592b727a7b95f6
n221199515 <hong_ronald@hotmail.com>
2019-06-15 07:17:27 -0500

    minor improvment

--- src/components/metricList.js
+++ src/components/metricList.js
@@ -3,7 +3,7 @@ import { Provider, createClient, useQuery } from "urql";
 import { useDispatch, useSelector } from "react-redux";
 import * as actions from "../store/actions";
 import CircularProgress from "@material-ui/core/LinearProgress";
-import MetricSelect from './MetricSelect'
+import { Dropdown } from 'semantic-ui-react'
 
 const client = createClient({
   url: "https://react.eogresources.com/graphql"
@@ -46,10 +46,16 @@ const MetricList = () => {
     const getMetrics = data;
     dispatch({ type: actions.METRICS_DATA_RECEIVED, getMetrics });
   }, [dispatch, data, error]);
-  console.log("getMetrics", getMetrics);
+  
   if (!getMetrics) return <CircularProgress />;
   
+  let options = []
+  getMetrics.map(value => {
+    let obj = { key: value, text: value, value: value }
+    options.push(obj)
+  })
+  
   return (
-    <MetricSelect  metricList={getMetrics}/>
-  );
+    <Dropdown placeholder='Select...' fluid multiple selection options={options}  style={{width: '500px'}}/>
+  )
 };
